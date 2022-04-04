95115862945836be062e6d83d4cb6435232cb056
tattip <ville.m.ollila@tuni.fi>
2022-04-04 19:05:36 +0300

    Initial commit

---
+++ Versionhallinta/Source.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+//aliohjelmat
+float sum(float x, float y) {
+	return x + y;
+};
+
+int main() {
+	char op;
+	float num1, num2;
+	cout << "Enter first number ";
+	cin >> num1;
+	cout << "Enter second number ";
+	cin >> num2;
+	cout << "Select operation \n"
+		"+ = addition \n ";
+		"- = substraction \n";
+		"* = multiplication \n";
+		"/ = division \n";
+	cin >> op;
+
+
+	switch (op) {
+	case '+':
+		cout << "You chose addition \n";
+		cout << num1 << " + " << num2 << " = " << sum(num1, num2) << endl;
+		break;
+	}
+};
